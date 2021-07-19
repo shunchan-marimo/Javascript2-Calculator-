7d0c016d7b734bbf0b38be9974607bb638b60f8b
EC2 Default User <ec2-user@ip-172-31-28-118.us-east-2.compute.internal>
2021-07-19 23:30:50 +0000

    ファイル追加

--- calc.js
+++ calc.js
@@ -1,14 +1,22 @@
 // DOM
 let result = document.getElementById("result");
+
 result.value = '0';
+
+let mark = document.getElementsByClassName("mark")
+let count = 0;
+let resultSearch = '+'/'-'/'*'/'/';
 
 // 関数    
 function edit(elem) {
-    if(result.value =='0'){
+     if(result.value =='0') {
      result.value = elem.value;
-    }else{
-     result.value = result.value + elem.value;
-    }
+     }else if(result.value === mark){
+     elem.value !== mark ;
+     result.value = result.value + elem.value  ;
+     }else{
+     
+     }
 }
 function calc() {
     result.value = new Function("return " + result.value)();
